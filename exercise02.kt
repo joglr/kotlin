2a7dce7cdcc74ad777773a5da5556fbdb5542a06
joglr <1959615+joglr@users.noreply.github.com>
2022-02-08 12:47:00 +0100

    Solve exercise 02.4

--- exercise02.kt
+++ exercise02.kt
@@ -3,21 +3,28 @@ import java.io.File
 data class Person(val name: String, val age: Int, var mother: Person?)
 
 fun main(args: Array<String>) {
+  var peopleMap = mapOf<String, Person>("God" to Person("God", Int.MAX_VALUE, null))
+  var people =
+      File("./genealogy.txt")
+          .readLines()
+          .map({
+            val row = it.split("\t")
+            val p = Person(row[0], row[1].toInt(), null)
+            peopleMap += p.name to p
+            val m = peopleMap[row[2]]
+            if (m != null) {
+              p.mother = m
+            }
+            p
+          })
+  // println("People")
+  // println(people)
 
+  println("Descendents of Eve")
 
-  var peopleMap =
-    mapOf<String, Person>("God" to Person("God", Int.MAX_VALUE, null))
-  var people = File("./genealogy.txt")
-    .readLines()
-    .map({
-      val row = it.split("\t")
-      val p = Person(row[0], row[1].toInt(), null)
-      peopleMap += p.name to p
-      val m = peopleMap[row[2]]
-      if (m != null) {
-        p.mother = m
-      }
-      p
-    })
-  println(people)
+  val descendents = people.filter({ it.mother === peopleMap["Eve"] })
+
+  for (d in descendents) {
+    println(d.name)
+  }
 }
