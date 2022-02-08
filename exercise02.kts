a0665b30261e90d97d43f8e1d8647e48af21d222
joglr <1959615+joglr@users.noreply.github.com>
2022-02-08 12:37:49 +0100

    Solve exercise 02.8

--- exercise02.kts
+++ exercise02.kts
@@ -1,8 +1,21 @@
-data class Person(val name: String, val age: Int, val mother: Person?)
-val name = "Jonas"
-val age = 24
-val person = Person(name, age, Person("mum", 50, null))
+import java.io.File
 
-println(person)
+data class Person(val name: String, val age: Int, var mother: Person?)
 
+
+var peopleMap =
+  mapOf<String, Person>("God" to Person("God", Int.MAX_VALUE, null))
+var people = File(args[0])
+  .readLines()
+  .map({
+    val row = it.split("\t")
+    val p = Person(row[0], row[1].toInt(), null)
+    peopleMap += p.name to p
+    val m = peopleMap[row[2]]
+    if (m != null) {
+      p.mother = m
+    }
+    p
+  })
+println(people)
 
